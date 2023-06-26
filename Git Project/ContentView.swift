795d8a266994a9a5e4c4459a0a5e6d3dc69051cb
David Lin <tiweilin20@gamil.com>
2023-06-26 00:19:52 -0700

    Created separate modal view

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Button("Show Modal") {
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented) {
+                ModalView()
+            }
+            .navigationBarTitle("Xcode and Git")
         }
-        .padding()
     }
 }
 
